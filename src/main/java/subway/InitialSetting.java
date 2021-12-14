72c6deb1c41ec94778b4b775b7c071be5488a7cc
sun <syoun602@gmail.com>
2021-12-14 21:44:10 +0900

    feat: initialize section values

--- src/main/java/subway/InitialSetting.java
+++ src/main/java/subway/InitialSetting.java
@@ -1,9 +1,6 @@
 package subway;
 
-import subway.domain.Line;
-import subway.domain.LineRepository;
-import subway.domain.Station;
-import subway.domain.StationRepository;
+import subway.domain.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,12 +9,52 @@ import java.util.stream.Collectors;
 public class InitialSetting {
     private static final List<String> stationNameList = Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
     private static final List<String> secondLineStationNameList = Arrays.asList("교대역", "강남역", "역삼역");
+    private static final int[][] secondLineStationCostList = {{2, 3}, {2, 3}};
     private static final List<String> thirdLineStationNameList = Arrays.asList("교대역", "남부터미널역", "양재역", "매봉역");
+    private static final int[][] thirdLineStationCostList = {{3, 2}, {6, 5}, {1, 1}};
     private static final List<String> shinBundangLineStationNameList = Arrays.asList("강남역", "양재역", "양재시민의숲역");
+    private static final int[][] shinBundangLineCostList = {{2, 8}, {10, 3}};
 
     public static void initialize() {
         insertStations();
         insertLines();
+        insertSection();
+    }
+
+    private static void insertSection() {
+        insertSecondSection();
+        insertThirdSection();
+        insertShinBundangSection();
+    }
+
+    private static void insertSecondSection() {
+        for (int i = 0; i < secondLineStationCostList.length; i++) {
+            SectionRepository.addSection(new Section(
+                    StationRepository.findByName(secondLineStationNameList.get(i)),
+                    StationRepository.findByName(secondLineStationNameList.get(i+1)),
+                    secondLineStationCostList[i][0],
+                    secondLineStationCostList[i][0]));
+        }
+    }
+
+    private static void insertThirdSection() {
+        for (int i = 0; i < secondLineStationCostList.length; i++) {
+            SectionRepository.addSection(new Section(
+                    StationRepository.findByName(thirdLineStationNameList.get(i)),
+                    StationRepository.findByName(thirdLineStationNameList.get(i+1)),
+                    thirdLineStationCostList[i][0],
+                    thirdLineStationCostList[i][0]));
+        }
+    }
+
+    private static void insertShinBundangSection() {
+        for (int i = 0; i < secondLineStationCostList.length; i++) {
+            SectionRepository.addSection(new Section(
+                    StationRepository.findByName(shinBundangLineStationNameList.get(i)),
+                    StationRepository.findByName(shinBundangLineStationNameList.get(i+1)),
+                    shinBundangLineCostList[i][0],
+                    shinBundangLineCostList[i][0]));
+        }
     }
 
     private static void insertStations() {
